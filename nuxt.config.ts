9a64903bbf4742d45691f693d43129736b759400
Agustin Amato <agustin.amato96@gmail.com>
2024-09-14 11:57:44 -0300

    chore: add global styles (css)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["vuetify-nuxt-module", "@nuxt/eslint"],
+  css: ["~/assets/css/main.scss", "~/assets/css/helpers.scss"],
 });
