d559cb5e139c4e3616ac730225f1957f244228cb
Agustin Amato <agustin.amato96@gmail.com>
2024-09-18 21:26:12 -0300

    chore: add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module", "@nuxt/eslint", "@pinia/nuxt"],
+  modules: [
+    "vuetify-nuxt-module",
+    "@nuxt/eslint",
+    "@pinia/nuxt",
+    "@nuxtjs/i18n",
+  ],
   css: ["~/assets/css/main.scss", "~/assets/css/helpers.scss"],
+  i18n: {
+    vueI18n: "./i18n.config.ts", // if you are using custom path, default
+  },
 });
