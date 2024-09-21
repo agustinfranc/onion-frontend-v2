33cf9b6b2b06be15e8b632e3ee911ca906925e9d
Agustin Amato <agustin.amato96@gmail.com>
2024-09-21 11:19:59 -0300

    feat: create app page: footer, theme and v-main

--- app.vue
+++ app.vue
@@ -1,6 +1,65 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-main>
+      <NuxtPage />
+    </v-main>
+
+    <v-footer app>
+      <div class="d-flex justify-space-between w-100">
+        <div class="text-left">
+          <span>&copy; 2020 - </span>
+          <span> {{ new Date().getFullYear() }}</span>
+        </div>
+        <div class="text-center">
+          <span>
+            made with
+            <v-icon>mdi-heart</v-icon> by
+            <router-link to="/">
+              <span
+                :class="
+                  theme.global.name.value === 'dark'
+                    ? 'white--text'
+                    : 'grey--text text--darken-4'
+                "
+                >Onion</span
+              >
+            </router-link>
+          </span>
+        </div>
+        <div class="text-right">
+          <a href="https://www.instagram.com/onion.com.ar/" target="_blank">
+            <v-icon class="mx-1" size="24px">mdi-instagram</v-icon>
+          </a>
+
+          <v-icon @click="toggleTheme">mdi-theme-light-dark</v-icon>
+        </div>
+      </div>
+    </v-footer>
+  </v-app>
 </template>
+
+<style scoped>
+.theme--dark.v-application .theme--parent {
+  background: #121212;
+  color: #ffffff;
+}
+
+.theme--light.v-application .theme--parent {
+  background: #f2f2f2;
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.v-app-bar--is-scrolled .v-app-bar-title__placeholder {
+  visibility: visible !important;
+}
+</style>
+
+<script setup lang="ts">
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
+</script>
