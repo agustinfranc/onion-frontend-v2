6bc10d3dc5b4b02a35f25ebc0f9ef53708f208d7
Agustin Amato <agustin.amato96@gmail.com>
2024-09-21 11:38:25 -0300

    feat: add template index page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout>
+    <div class="d-flex text-center">
+      <img alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </div>
+  </v-layout>
+</template>
