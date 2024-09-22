ac622fef42f68949188aa3f5c46f7b93aacef788
Agustin Amato <agustin.amato96@gmail.com>
2024-09-22 20:42:24 -0300

    feat: styles

--- app.vue
+++ app.vue
@@ -8,8 +8,10 @@
       <div class="d-flex justify-space-between w-100">
         <div class="text-left">
           <span>&copy; 2020 - </span>
+
           <span> {{ new Date().getFullYear() }}</span>
         </div>
+
         <div class="text-center">
           <span>
             made with
@@ -26,6 +28,7 @@
             </router-link>
           </span>
         </div>
+
         <div class="text-right">
           <a href="https://www.instagram.com/onion.com.ar/" target="_blank">
             <v-icon class="mx-1" size="24px">mdi-instagram</v-icon>
@@ -39,16 +42,6 @@
 </template>
 
 <style scoped>
-.theme--dark.v-application .theme--parent {
-  background: #121212;
-  color: #ffffff;
-}
-
-.theme--light.v-application .theme--parent {
-  background: #f2f2f2;
-  color: rgba(0, 0, 0, 0.87);
-}
-
 .v-app-bar--is-scrolled .v-app-bar-title__placeholder {
   visibility: visible !important;
 }
