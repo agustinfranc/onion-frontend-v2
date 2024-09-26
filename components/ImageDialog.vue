dacad7a61e6d628763a27154147205cc2a27cc8e
Agustin Amato <agustin.amato96@gmail.com>
2024-09-26 16:26:26 -0300

    feat: add image dialog

---
+++ components/ImageDialog.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-dialog v-model="show">
+    <v-img
+      v-if="dialog.show"
+      :lazy-src="dialog.imgLazySrc"
+      :src="dialog.imgSrc"
+    >
+      <v-app-bar variant="flat" color="rgba(0, 0, 0, 0)">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          color="black"
+          icon
+          @click.stop="$emit('update:modelValue', false)"
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-app-bar>
+
+      <template #placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular
+            indeterminate
+            color="grey lighten-5"
+          ></v-progress-circular>
+        </v-row> </template
+    ></v-img>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+    dialog: {
+      type: Object,
+      required: true,
+    },
+  },
+  emits: ["update:modelValue"],
+  computed: {
+    show: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+};
+</script>
