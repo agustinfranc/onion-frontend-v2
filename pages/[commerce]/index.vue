c2c518bc21ea571f0573317b6231966afc8fb27f
Agustin Amato <agustin.amato96@gmail.com>
2024-10-02 17:32:34 -0300

    fix: do not call commerce endpoint twice

--- pages/[commerce]/index.vue
+++ pages/[commerce]/index.vue
@@ -23,78 +23,85 @@ const { t } = useI18n();
 const config = useRuntimeConfig();
 const route = useRoute();
 const commerce = useState<Commerce>("commerce");
+
 const cart = useState<any[]>("cart");
 
-// Calls fast endpoint in the server side to get commerce fullname and covers
-const { data: commerceMetadata } = await useFetch(
-  `${config.public.apiUrl}/${route.params.commerce}?simplified=true`,
-  {
-    key: "commerceMetadata",
-  }
-);
+if (!cart.value) {
+  cart.value = [];
+}
 
-// Calls full commerce endpoint
-useLazyFetch<Commerce>(`${config.public.apiUrl}/${route.params.commerce}`, {
-  key: "commerce",
-  server: false,
-  deep: false,
-  dedupe: "defer",
-  onResponse({ response }) {
-    if (response?._data) {
-      commerce.value = response._data;
-      theme.global.name.value = response._data.dark_theme ? "dark" : "light";
+if (!commerce.value) {
+  // Calls fast endpoint in the server side to get commerce fullname and covers
+  const { data: commerceMetadata } = await useFetch(
+    `${config.public.apiUrl}/${route.params.commerce}?simplified=true`,
+    {
+      key: "commerceMetadata",
     }
-  },
-});
+  );
 
-commerceMetadata.value &&
-  useHead({
-    title: `${commerceMetadata.value.fullname} | Onion`,
-    titleTemplate: `${commerceMetadata.value.fullname} | Onion`,
-    meta: [
-      {
-        hid: "description",
-        name: "description",
-        content: `${commerceMetadata.value.fullname} | ${t(
-          "Access to our digital menu"
-        )} | ${t("Developed by Onion")}`,
-      },
-      {
-        hid: "og:title",
-        property: "og:title",
-        content: `${commerceMetadata.value.fullname} | Onion`,
-      },
-      {
-        hid: "og:host",
-        property: "og:host",
-        content: `https://onion.ar/${commerceMetadata.value.name}`,
-      },
-      {
-        hid: "og:url",
-        property: "og:url",
-        content: `https://onion.ar/${commerceMetadata.value.name}`,
-      },
-      {
-        hid: "og:image",
-        property: "og:image",
-        content: commerceMetadata.value.cover_dirname,
-      },
-      {
-        hid: "og:description",
-        property: "og:description",
-        content: `${commerceMetadata.value.fullname} | ${t(
-          "Access to our digital menu"
-        )} | ${t("Developed by Onion")}`,
-      },
-    ],
-    link: [
-      {
-        hid: "canonical",
-        rel: "canonical",
-        href: `https://onion.ar/${commerceMetadata.value.name}`,
-      },
-    ],
+  // Calls full commerce endpoint
+  useLazyFetch<Commerce>(`${config.public.apiUrl}/${route.params.commerce}`, {
+    key: "commerce",
+    server: false,
+    deep: false,
+    dedupe: "defer",
+    onResponse({ response }) {
+      if (response?._data) {
+        commerce.value = response._data;
+        theme.global.name.value = response._data.dark_theme ? "dark" : "light";
+      }
+    },
   });
+
+  commerceMetadata.value &&
+    useHead({
+      title: `${commerceMetadata.value.fullname} | Onion`,
+      titleTemplate: `${commerceMetadata.value.fullname} | Onion`,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: `${commerceMetadata.value.fullname} | ${t(
+            "Access to our digital menu"
+          )} | ${t("Developed by Onion")}`,
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: `${commerceMetadata.value.fullname} | Onion`,
+        },
+        {
+          hid: "og:host",
+          property: "og:host",
+          content: `https://onion.ar/${commerceMetadata.value.name}`,
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: `https://onion.ar/${commerceMetadata.value.name}`,
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: commerceMetadata.value.cover_dirname,
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: `${commerceMetadata.value.fullname} | ${t(
+            "Access to our digital menu"
+          )} | ${t("Developed by Onion")}`,
+        },
+      ],
+      link: [
+        {
+          hid: "canonical",
+          rel: "canonical",
+          href: `https://onion.ar/${commerceMetadata.value.name}`,
+        },
+      ],
+    });
+}
 </script>
 
 <style>
