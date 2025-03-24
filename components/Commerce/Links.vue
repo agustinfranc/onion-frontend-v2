96a72a38fcc00b83455458306188bd47d58429e5
Agustin Amato <agustin.amato96@gmail.com>
2025-03-24 11:17:59 -0300

    fix: tiktok link issue

--- components/Commerce/Links.vue
+++ components/Commerce/Links.vue
@@ -30,7 +30,7 @@
 
     <a
       v-if="commerce.tiktok_account"
-      :href="`https://vm.tiktok.com/${commerce.tiktok_account}/`"
+      :href="`https://tiktok.com/${commerce.tiktok_account}/`"
       target="_blank"
       style="position: relative; top: 4px"
     >
@@ -38,7 +38,7 @@
         width="22"
         height="22"
         src="https://sf16-scmcdn-va.ibytedtos.com/goofy/tiktok/web/node/_next/static/images/logo-dark-e95da587b6efa1520dcd11f4b45c0cf6.svg"
-      >
+      />
     </a>
 
     <a
